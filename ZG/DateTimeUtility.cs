5a944e81150160247b2dd774cf3b2429839c8c5f
linfuqing <linfuqing@126.com>
2025-06-25 16:53:38 +0800

    version 2.2.97

---
+++ ZG/DateTimeUtility.cs
@@ -0,0 +1,47 @@
+using System;
+
+namespace ZG
+{
+    public static class DateTimeUtility
+    {
+        public static readonly DateTime Utc1970 = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+
+        public static uint GetSeconds()
+        {
+            return (uint)((DateTime.UtcNow.Ticks - Utc1970.Ticks) / TimeSpan.TicksPerSecond);
+        }
+
+        public static int GetTotalDays(uint seconds, out DateTime dateTime, out DateTime now)
+        {
+            dateTime = new DateTime(seconds * TimeSpan.TicksPerSecond + Utc1970.Ticks).ToLocalTime();
+
+            now = DateTime.Now;
+
+            return (int)(now - dateTime).TotalDays;
+        }
+
+        public static bool IsToday(uint seconds)
+        {
+            return GetTotalDays(seconds, out _, out _) > 0;
+        }
+
+        public static bool IsThisWeek(uint seconds)
+        {
+            var totalDays = GetTotalDays(seconds, out var dateTime, out var now);
+            if (totalDays < 7 && totalDays > -7)
+            {
+                DayOfWeek dayOfWeek = dateTime.DayOfWeek, nowDayOfWeek = now.DayOfWeek;
+                if ((totalDays >= 0.0f) ^ (dayOfWeek >= nowDayOfWeek))
+                    return true;
+            }
+
+            return false;
+        }
+
+        public static bool IsThisMonth(uint seconds)
+        {
+            var totalDays = GetTotalDays(seconds, out var dateTime, out var now);
+            return totalDays < 30 && totalDays > -30 && dateTime.Month == now.Month;
+        }
+    }
+}
