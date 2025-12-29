644e36aa0fbdc5fb430d4ea528b64368b44026f0
linfuqing <linfuqing@126.com>
2025-12-29 17:30:04 +0800

    version 3.1.5

--- ZG/DateTimeUtility.cs
+++ ZG/DateTimeUtility.cs
@@ -4,19 +4,25 @@ namespace ZG
 {
     public static class DateTimeUtility
     {
-        public static readonly DateTime Utc1970 = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+        public enum DataTimeType
+        {
+            UTC, 
+            Local
+        }
+        
+        public static readonly DateTime UTC1970 = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
 
         public static long GetTicks(uint seconds)
         {
             if (seconds == 0)
                 return 0;
             
-            return seconds * TimeSpan.TicksPerSecond + Utc1970.Ticks;
+            return seconds * TimeSpan.TicksPerSecond + UTC1970.Ticks;
         }
         
         public static uint GetSeconds(long ticks)
         {
-            return (uint)((ticks - Utc1970.Ticks) / TimeSpan.TicksPerSecond);
+            return (uint)((ticks - UTC1970.Ticks) / TimeSpan.TicksPerSecond);
         }
 
         public static uint GetSeconds()
@@ -24,23 +30,33 @@ namespace ZG
             return GetSeconds(DateTime.UtcNow.Ticks);
         }
 
-        public static int GetTotalDays(uint seconds, out DateTime dateTime, out DateTime now)
+        public static int GetTotalDays(uint seconds, out DateTime dateTime, out DateTime now, DataTimeType type)
         {
-            dateTime = new DateTime(GetTicks(seconds)).ToLocalTime();
+            switch (type)
+            {
+                case DataTimeType.Local:
+                    dateTime = new DateTime(GetTicks(seconds)).ToLocalTime();
 
-            now = DateTime.Now;
-
+                    now = DateTime.Now;
+                    break;
+                default:
+                    dateTime = new DateTime(GetTicks(seconds));
+                    
+                    now = DateTime.UtcNow;
+                    break;
+            }
+            
             return now.Day - dateTime.Day;
         }
 
-        public static bool IsToday(uint seconds)
+        public static bool IsToday(uint seconds, DataTimeType type)
         {
-            return Math.Abs(GetTotalDays(seconds, out _, out _)) < 1;
+            return Math.Abs(GetTotalDays(seconds, out _, out _, type)) < 1;
         }
 
-        public static bool IsThisWeek(uint seconds)
+        public static bool IsThisWeek(uint seconds, DataTimeType type)
         {
-            int totalDays = GetTotalDays(seconds, out var dateTime, out var now);
+            int totalDays = GetTotalDays(seconds, out var dateTime, out var now, type);
             if (Math.Abs(totalDays) < 7)
             {
                 DayOfWeek dayOfWeek = dateTime.DayOfWeek, nowDayOfWeek = now.DayOfWeek;
@@ -50,9 +66,9 @@ namespace ZG
             return false;
         }
 
-        public static bool IsThisMonth(uint seconds)
+        public static bool IsThisMonth(uint seconds, DataTimeType type)
         {
-            int totalDays = GetTotalDays(seconds, out var dateTime, out var now);
+            int totalDays = GetTotalDays(seconds, out var dateTime, out var now, type);
             return Math.Abs(totalDays) < 30 && dateTime.Month == now.Month;
         }
     }
